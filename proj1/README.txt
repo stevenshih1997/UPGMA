0fc291ef3ec0eec0116c69735ce179076c921cf1
Steven Shih <stevenshih1997@gmail.com>
2017-06-02 16:08:04 -0700

    Project

---
+++ proj1/README.txt
@@ -0,0 +1,14 @@
+Steven Shih
+Hanah Park
+
+This project took approximately 20 hours to complete.
+
+I used C++ documentation on utilizing a vector
+
+List of files in submission:
+dynmatrix.h: contains headers of functions used in dynmatrix.cc
+dynmatrix.cc: contains function bodies with working code
+main.cc: calls functions to perform UPGMA algorithm and prints expected output
+Makefile: compiles the project by calling make
+species.txt: sample input to run project with expected output
+
---
+++ proj1/main.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "dynmatrix.h"
+
+using namespace std;
+
+// construct a string representation 
+
+void usage() {
+  cerr << "Usage: upgma species.txt" << endl;
+}
+
+int main( int argc, char *argv[] ) {
+  if( argc != 2 ) {
+    usage();
+    return 0;
+  }
+
+  // let's start with empty DynMatrix:
+  ClusterNode* head = NULL; 
+  ClusterNode* tail = NULL;
+  int n = 0; // number of species
+  ifstream fin( argv[1] );
+  if( fin.is_open() ) {
+    // read species:
+    string species;
+    while ((fin.peek() != '\n') && (fin >> species)) {
+      n++;
+     // addCluster adds empty species into DynMatrix 
+      addCluster(head, tail, species);
+    }
+    // Adding distances to the DynMatrix initialized to zeroes
+    ClusterNode *node = head;
+    for (int i=0; i<n && fin; i++) {
+      DistanceNode *newDistance = node->row;
+      for (int j=0; j<n && fin; j++) {
+        double d;
+        fin >> d;
+
+        newDistance->distance = d;
+
+        newDistance = newDistance->nextInRow;
+      }
+    node  = node->next;
+    }
+    fin.close();
+  }
+  // Run UPGMA until there is only one node left
+  while (head->next) {
+    UPGMA(head,tail);
+  }
+  // Print out name of last node
+  if (head) {
+    cout  << head->name << endl;
+  }
+  // BONUS (optional): print the tree in a nice way
+
+  return 0;
+}
